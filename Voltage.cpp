8f0eef9aaee130e8f5ca0ad19a752f4a8c3e807d
Jonas Genannt <jonas@brachium-system.net>
2020-05-20 09:50:56 +0200

    add internal voltage read from attiny85

--- Voltage.cpp
+++ Voltage.cpp
@@ -1,4 +1,4 @@
-#if defined(__AVR_ATmega328P__)
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATtiny85__)
 #include <avr/power.h>
 #include "Voltage.h"
 
@@ -8,20 +8,30 @@ void Voltage::init(void) {
 }
 
 void Voltage::enable(void) {
+#if defined(__AVR_ATmega328P__)
   power_adc_enable();
+#endif
   ADCSRA = ADCSRA_status;
 }
 
 void Voltage::disable(void) {
   ADCSRA &= ~(1 << 7);
+#if defined(__AVR_ATmega328P__)
   power_adc_disable();
+#endif
 }
 
 float Voltage::read(void) {
   enable();
 
+#if defined(__AVR_ATmega328P__)
   ADCSRA |= bit (ADPS0) |  bit (ADPS1) | bit (ADPS2);  // Prescaler of 128
   ADMUX = bit (REFS0) | bit (MUX3) | bit (MUX2) | bit (MUX1);
+#elif defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+  delay(2);
+  ADCSRA |= _BV(ADSC);
+#endif
 
   delay(10);
   bitSet (ADCSRA, ADSC);
